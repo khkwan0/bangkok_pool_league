82b34f294f582e2d57051a804cd21fee064c7cfa
Kenneth Kwan <khkwan0@gmail.com>
2024-01-09 11:10:54 +0700

    updates

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -133,119 +133,117 @@ const Login = props => {
         </SafeAreaView>
       )}
       {(typeof user.data?.nickname === 'undefined' || !user.data.nickname) && (
-          <View flex={1} bgColor={colors.background} px={20}>
-            <View flex={1} mt={20}>
-              <Text bold textAlign="center" fontSize="xxl">
-                Bangkok Pool League
-              </Text>
-              {colorMode === 'light' && (
+        <View flex={1} bgColor={colors.background} px={20}>
+          <View flex={1} mt={20}>
+            <Text bold textAlign="center" fontSize="xxl">
+              Bangkok Pool League
+            </Text>
+            {colorMode === 'light' && (
+              <View>
+                <Image source={Logo} />
+              </View>
+            )}
+          </View>
+          <View flex={10}>
+            <View>
+              <View>
+                <Button variant="ghost" onPress={() => setShowSocial(s => !s)}>
+                  {t('social_login')}
+                </Button>
+              </View>
+              {showSocial && (
                 <View>
-                  <Image source={Logo} />
+                  <View mx={40}>
+                    <Button onPress={() => HandleFacebookLogin()}>
+                      Facebook
+                    </Button>
+                  </View>
+                  <View mx={40} mt={10}>
+                    <Pressable
+                      onPress={() => HandleLineLogin()}
+                      borderRadius={theme.roundness}
+                      bgColor="#06c755"
+                      py={10}>
+                      <Row alignItems="center" space={30}>
+                        <View flex={1} pl={10}>
+                          <Image
+                            source={require('~/assets/social/line/btn_base.png')}
+                          />
+                        </View>
+                        <View flex={2} alignItems="center">
+                          <Text bold color="#fff" fontSize="xl">
+                            Line
+                          </Text>
+                        </View>
+                        <View flex={1} />
+                      </Row>
+                    </Pressable>
+                  </View>
                 </View>
               )}
             </View>
-            <View flex={10}>
-              <View>
-                <View>
-                  <Button
-                    variant="ghost"
-                    onPress={() => setShowSocial(s => !s)}>
-                    {t('social_login')}
-                  </Button>
-                </View>
-                {showSocial && (
-                  <View>
-                    <View mx={40}>
-                      <Button onPress={() => HandleFacebookLogin()}>
-                        Facebook
-                      </Button>
-                    </View>
-                    <View mx={40} mt={10}>
-                      <Pressable
-                        onPress={() => HandleLineLogin()}
-                        borderRadius={theme.roundness}
-                        bgColor="#06c755"
-                        py={10}>
-                        <Row alignItems="center" space={30}>
-                          <View flex={1} pl={10}>
-                            <Image
-                              source={require('~/assets/social/line/btn_base.png')}
-                            />
-                          </View>
-                          <View flex={2} alignItems="center">
-                            <Text bold color="#fff" fontSize="xl">
-                              Line
-                            </Text>
-                          </View>
-                          <View flex={1} />
-                        </Row>
-                      </Pressable>
-                    </View>
+            <View mt={20}>
+              <TextInput
+                autoCapitalize="none"
+                placeholder={t('email')}
+                value={email}
+                inputLeftElement={
+                  <View ml={10}>
+                    <MCI name="email" size={30} color={colors.onSurface} />
                   </View>
-                )}
-              </View>
-              <View mt={20}>
-                <TextInput
-                  autoCapitalize="none"
-                  placeholder={t('email')}
-                  value={email}
-                  inputLeftElement={
-                    <View ml={10}>
-                      <MCI name="email" size={30} color={colors.onSurface} />
-                    </View>
-                  }
-                  onChangeText={text => setEmail(text)}
-                />
-              </View>
-              <View mt={20}>
-                <TextInput
-                  secureTextEntry={secure}
-                  autoCapitalize="none"
-                  placeholder={t('password')}
-                  value={password}
-                  inputLeftElement={
-                    <View ml={10}>
-                      <MCI name="lock" size={30} color={colors.onSurface} />
-                    </View>
-                  }
-                  inputRightElement={
-                    <Pressable onPress={() => setSecure(s => !s)} pr={10}>
-                      <MCI
-                        name={secure ? 'eye-outline' : 'eye-off-outline'}
-                        size={30}
-                        color={colors.onSurface}
-                      />
-                    </Pressable>
-                  }
-                  onChangeText={text => setPassword(text)}
-                />
-              </View>
-              <View mt={20}>
-                <View>
-                  <Button
-                    loading={loading}
-                    disabled={loading}
-                    onPress={() => AttemptLogin()}>
-                    {t('login')}
-                  </Button>
-                </View>
-              </View>
+                }
+                onChangeText={text => setEmail(text)}
+              />
+            </View>
+            <View mt={20}>
+              <TextInput
+                secureTextEntry={secure}
+                autoCapitalize="none"
+                placeholder={t('password')}
+                value={password}
+                inputLeftElement={
+                  <View ml={10}>
+                    <MCI name="lock" size={30} color={colors.onSurface} />
+                  </View>
+                }
+                inputRightElement={
+                  <Pressable onPress={() => setSecure(s => !s)} pr={10}>
+                    <MCI
+                      name={secure ? 'eye-outline' : 'eye-off-outline'}
+                      size={30}
+                      color={colors.onSurface}
+                    />
+                  </Pressable>
+                }
+                onChangeText={text => setPassword(text)}
+              />
+            </View>
+            <View mt={20}>
               <View>
                 <Button
-                  variant="ghost"
-                  onPress={() => props.navigation.navigate('RegisterPart1')}>
-                  {t('sign_up')}
+                  loading={loading}
+                  disabled={loading}
+                  onPress={() => AttemptLogin()}>
+                  {t('login')}
                 </Button>
               </View>
             </View>
-            <View flex={1} pb={insets.bottom}>
+            <View>
               <Button
-                onPress={() => props.navigation.navigate('Recover')}
-                variant="ghost">
-                {t('forgot_password')}
+                variant="ghost"
+                onPress={() => props.navigation.navigate('RegisterPart1')}>
+                {t('sign_up')}
               </Button>
             </View>
           </View>
+          <View flex={1} pb={insets.bottom}>
+            <Button
+              onPress={() => props.navigation.navigate('Recover')}
+              variant="ghost">
+              {t('forgot_password')}
+            </Button>
+          </View>
+        </View>
       )}
     </>
   )
