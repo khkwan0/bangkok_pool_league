7aa23a330d1bbcd1b16c2bfb540d05831be66f6b
Kenneth Kwan <khkwan0@gmail.com>
2024-01-14 19:08:14 +0700

    fixes

--- src/screens/Auth/PostRecover.js
+++ src/screens/Auth/PostRecover.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import {Button, Text, View} from '@ybase'
 import {useYBase} from '~/lib/hooks'
-import {useTranslations} from 'react-i18next'
+import MCI from 'react-native-vector-icons/MaterialCommunityIcons'
+import {useTranslation} from 'react-i18next'
 
 const PostRecover = props => {
   const {colors} = useYBase()
-  const {t} = useTranslations()
+  const {t} = useTranslation()
   return (
     <View flex={1} px={20} bgColor={colors.background}>
       <View flex={1} />
       <View flex={4}>
-        <Text bold fontSize="xxxl" textAlign="center">success_password_reset</Text>
+        <Text bold fontSize="xxl" textAlign="center">
+          password_reset
+        </Text>
+        <MCI name="check-circle" color={colors.green['500']} size={45} />
+        <Text bold fontSize="xxxl" textAlign="center">
+          success
+        </Text>
       </View>
       <View flex={1}>
         <Button>{t('continue')}</Button>
