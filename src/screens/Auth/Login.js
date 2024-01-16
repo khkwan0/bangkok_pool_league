7e08f88b3ec324f5c900e680be9cf5bb3603f5a9
Kenneth Kwan <khkwan0@gmail.com>
2024-01-16 12:11:00 +0700

    apple login

--- src/screens/Auth/Login.js
+++ src/screens/Auth/Login.js
@@ -7,6 +7,7 @@ import LineLogin from '@xmartlabs/react-native-line'
 import LineSuccess from './LineSuccess'
 import {Settings, LoginManager, AccessToken} from 'react-native-fbsdk-next'
 import {AppleButton} from '@invertase/react-native-apple-authentication'
+import {appleAuth} from '@invertase/react-native-apple-authentication'
 import {Platform} from 'react-native'
 import {useWindowDimensions} from 'react-native'
 /*
@@ -141,7 +142,26 @@ const Login = props => {
     }
   }
 
-  async function HandleAppleSignIn() {}
+  async function HandleAppleSignIn() {
+    // performs login request
+    const appleAuthRequestResponse = await appleAuth.performRequest({
+      requestedOperation: appleAuth.Operation.LOGIN,
+      // Note: it appears putting FULL_NAME first is important, see issue #293
+      requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
+    })
+
+    // get current authentication state for user
+    // /!\ This method must be tested on a real device. On the iOS simulator it always throws an error.
+    const credentialState = await appleAuth.getCredentialStateForUser(
+      appleAuthRequestResponse.user,
+    )
+
+    // use credentialState response to ensure the user is authenticated
+    if (credentialState === appleAuth.State.AUTHORIZED) {
+      console.log('authenticated')
+      // user is authenticated
+    }
+  }
 
   if (lineSuccess) {
     return <LineSuccess />
