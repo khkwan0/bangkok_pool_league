37c9caed888c6295880e16a9a6777efe77c1b42d
Kenneth Kwan <khkwan0@gmail.com>
2024-01-23 18:12:31 +0700

    fixes

--- src/components/SeasonPicker.js
+++ src/components/SeasonPicker.js
@@ -3,9 +3,13 @@ import RNPickerSelect from 'react-native-picker-select'
 import {Picker} from '@react-native-picker/picker'
 import {useLeague, useYBase} from '~/lib/hooks'
 import {useTranslation} from 'react-i18next'
+import {Platform} from 'react-native'
+import {Pressable, Row, Text} from '@ybase'
 
 const SeasonPicker = props => {
   const [seasons, setSeasons] = React.useState([])
+  const [open, setOpen] = React.useState(false)
+  const [season, setSeason] = React.useState(11)
   const league = useLeague()
   const {t} = useTranslation()
   const {colors} = useYBase()
@@ -23,46 +27,59 @@ const SeasonPicker = props => {
     })()
   }, [])
 
-  return (
-    <RNPickerSelect
-      onValueChange={val => props.setSeason(val)}
-      items={seasons.map(_season => ({
-        label: `${t('season')} ${
-          _season.identifier ? _season.identifier : _season.id
-        } ${_season.name}`,
-        value: _season.id,
-        color: colors.onSurface,
-      }))}
-      value={props.season}
-      dropdownIconColor={colors.onSurface}
-      pickerProps={{dropdownIconColor: colors.onSurface}}
-    />
-  )
-  /*
-  return (
-    <Picker
-      dropdownIconColor={colors.onSurface}
-      selectedValue={props.season}
-      mode="dropdown"
-      onValueChange={(itemValue, itemPosition) => props.setSeason(itemValue)}>
-      {seasons.map(_season => (
-        <Picker.Item
-          color={colors.onSurface}
-          style={{backgroundColor: colors.surface}}
-          key={_season.id}
-          label={
-            t('season') +
-            ' ' +
-            (_season.identifier
-              ? _season.identifier
-              : _season.id + ' ' + _season.name)
-          }
-          value={_season.id}
-        />
-      ))}
-    </Picker>
-  )
-  */
+  const items = seasons.map(_season => ({
+    label: `${t('season')} ${
+      _season.identifier ? _season.identifier : _season.id
+    } ${_season.name}`,
+    value: _season.id,
+    color: colors.onSurface,
+  }))
+
+  const _season = seasons.find(s => s.id === season)
+
+  if (_season) {
+    if (Platform.OS === 'ios' && !open) {
+      return (
+        <Pressable onPress={() => setOpen(true)}>
+          <Row alignItems="center">
+            <Text>
+              {t('season')}{' '}
+              {season.identifier ? _season.identifier : _season.id}{' '}
+              {_season.name}
+            </Text>
+          </Row>
+        </Pressable>
+      )
+    } else if (Platform.OS === 'android' || open) {
+      return (
+        <Picker
+          dropdownIconColor={colors.onSurface}
+          selectedValue={props.season}
+          mode="dropdown"
+          onValueChange={(itemValue, itemPosition) =>
+            props.setSeason(itemValue)
+          }>
+          {seasons.map(_season => (
+            <Picker.Item
+              color={colors.onSurface}
+              style={{backgroundColor: colors.surface}}
+              key={_season.id}
+              label={
+                t('season') +
+                ' ' +
+                (_season.identifier
+                  ? _season.identifier
+                  : _season.id + ' ' + _season.name)
+              }
+              value={_season.id}
+            />
+          ))}
+        </Picker>
+      )
+    }
+  } else {
+    return null
+  }
 }
 
 export default SeasonPicker
