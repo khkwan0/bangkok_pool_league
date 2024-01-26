8bc54ad6346c2044d366df84fdbbfd4eb19999b9
Kenneth Kwan <khkwan0@gmail.com>
2024-01-26 22:13:41 +0700

    fix

--- src/Main.js
+++ src/Main.js
@@ -40,9 +40,10 @@ const Main = props => {
   React.useEffect(() => {
     const subscription = AppState.addEventListener('change', nextAppState => {
       console.log(appState.current, nextAppState)
-      if (appState.current === 'active' && nextAppState === 'active') {
+      if (appState.current.match(/background|active/) && nextAppState === 'active') {
         FetchUser()
       }
+      appState.current = nextAppState
     })
     return () => subscription.remove()
   }, [])
