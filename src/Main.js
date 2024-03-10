4875fddd6038f1476ab004d5bddca613b1a4c678
Kenneth Kwan <khkwan0@gmail.com>
2024-03-10 13:54:15 +0700

    fixes

--- src/Main.js
+++ src/Main.js
@@ -13,59 +13,12 @@ import notifee, {AndroidImportance} from '@notifee/react-native'
 import MCI from 'react-native-vector-icons/MaterialCommunityIcons'
 import {useSafeAreaInsets} from 'react-native-safe-area-context'
 
-const NeedUpdate = props => {
-  const {colors} = useYBase()
-  const [acknowledged, setAcknowledged] = React.useState(false)
-  const insets = useSafeAreaInsets()
-
-  async function HandleUpdate() {
-    try {
-      setAcknowledged(true)
-      const url =
-        Platform.OS === 'ios'
-          ? 'https://apps.apple.com/us/app/bangkok-pool-league/id6447631894'
-          : 'https://play.google.com/store/apps/details?id=com.bangkok_pool_league'
-      const supported = await Linking.canOpenURL(url)
-      if (supported) {
-        await Linking.openURL(url)
-      }
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  if (!acknowledged && props.needsUpdate) {
-    return (
-      <Row alignItems="center" pt={insets.top}>
-        <View flex={1} />
-        <View flex={2}>
-          <Pressable
-            py={10}
-            bgColor={colors.error}
-            onPress={() => HandleUpdate()}>
-            <Text bold textAlign="center">
-              A new version is available.
-            </Text>
-          </Pressable>
-        </View>
-        <View flex={1} alignItems="flex-end">
-          <Pressable pr={20} onPress={() => setAcknowledged(true)}>
-            <MCI name="close" size={20} />
-          </Pressable>
-        </View>
-      </Row>
-    )
-  } else {
-    return null
-  }
-}
 const Main = props => {
   const account = useAccount()
   const league = useLeague()
   const [isMounted, setIsMounted] = React.useState(false)
   const {i18n} = useTranslation()
   const {colors, setColorMode} = useYBase()
-  const [needsUpdate, setNeedsUpdate] = React.useState(false)
 
   const appState = React.useRef(AppState.currentState)
 
@@ -84,7 +37,6 @@ const Main = props => {
 
   async function FetchUser() {
     try {
-      console.log('fetch user')
       await account.FetchUser()
     } catch (e) {
       console.log(e)
@@ -152,18 +104,10 @@ const Main = props => {
     CreateChannel()
   }, [])
 
-  async function CheckVersion() {
-    setNeedsUpdate(await account.CheckVersion())
-  }
-
-  React.useEffect(() => {
-    CheckVersion()
-  }, [])
 
   if (isMounted) {
     return (
       <View flex={1} bgColor={colors.background}>
-        <NeedUpdate needsUpdate={needsUpdate} />
         <Home />
       </View>
     )
